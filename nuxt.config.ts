6ae127827abe85008f93184ce4652923d35b0d3e
Eric Almendral <eralmendral@gmail.com>
2022-11-28 19:46:57 +0800

    add pinia state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,5 +21,8 @@ export default defineNuxtConfig({
         public: {
             API_BASE_URL: process.env.API_BASE_URL
         }
-    }
+    },
+    modules: [
+        '@pinia/nuxt',
+    ]
 })
